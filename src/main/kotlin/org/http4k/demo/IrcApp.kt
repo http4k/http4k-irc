f743af70bf75f804a54500db7012433983733dbe
David Denton <denton.david@gmail.com>
2017-12-01 15:15:32 +0000

    released lib, so upgrade and go!

--- src/main/kotlin/org/http4k/demo/IrcApp.kt
+++ src/main/kotlin/org/http4k/demo/IrcApp.kt
@@ -1,28 +1,32 @@
 package org.http4k.demo
 
 import io.github.konfigur8.Configuration
+import org.http4k.core.then
+import org.http4k.demo.Settings.CREDENTIALS
 import org.http4k.filter.ServerFilters
 import org.http4k.routing.ResourceLoader
-import org.http4k.routing.RoutingHttpHandler
 import org.http4k.routing.bind
 import org.http4k.routing.static
+import org.http4k.routing.websockets
+import org.http4k.websocket.PolyHandler
+import org.http4k.websocket.Websocket
+import org.http4k.websocket.WsMessage
 import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.atomic.AtomicInteger
-import javax.security.sasl.Sasl.CREDENTIALS
 
 
 object IrcApp {
-    operator fun invoke(config: Configuration): RoutingHttpHandler {
+    operator fun invoke(config: Configuration): PolyHandler {
         val userCounter = AtomicInteger()
         val messages = mutableListOf<String>()
-        val participants = ConcurrentHashMap<String, WebSocket>()
+        val participants = ConcurrentHashMap<String, Websocket>()
 
         fun addMessage(new: String) {
             messages.add(new)
             participants.values.forEach { it.send(WsMessage(new)) }
         }
 
-        fun newConnection(ws: WebSocket) {
+        fun newConnection(ws: Websocket) {
             val id = "user${userCounter.incrementAndGet()}"
             participants += id to ws
             addMessage("$id joined")
@@ -37,7 +41,8 @@ object IrcApp {
         }
 
         return PolyHandler(
-            ServerFilters.BasicAuth("http4k", config[CREDENTIALS]).then(static(ResourceLoader.Classpath())),
+            ServerFilters.BasicAuth("http4k", config[CREDENTIALS])
+                .then(static(ResourceLoader.Classpath())),
             websockets("/ws" bind ::newConnection)
         )
     }
